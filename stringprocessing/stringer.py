71c9f3c2d372a8786a0a51ed95722fb793af3295
Aurora Hamilton <prokriti@gmail.com>
2020-06-28 14:49:28 -0700

    First commit

---
+++ stringprocessing/stringer.py
@@ -0,0 +1,22 @@
+import string
+
+def string_chara_separator(string: str): 
+    i = 0
+    l = len(string)
+    string = list(string)
+    for i in range(0, l):
+        print(string[i])
+
+
+def reverse_string_chara_separator(string: str): 
+    l = len(string)
+    i = l - 1
+    string = list(string)
+    while i >= 0:
+        print(string[i])
+        i = i - 1
+
+print("Hello")
+s = "Abhinaba"
+string_chara_separator(s)
+reverse_string_chara_separator(s)
